7f528e27e8ec45f906622d3ad8f320e92ae1757d
soffiane <sylar93120@hotmail.fr>
2019-11-21 09:32:01 +0100

    last commit

---
+++ src/main/java/com/adneom/formatter/ListFormatter.java
@@ -0,0 +1,59 @@
+package com.adneom.formatter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+/**
+ * The type List formatter.
+ */
+public class ListFormatter {
+
+    /**
+     * Logger.
+     */
+    private static Logger logger = Logger.getLogger(ListFormatter.class.getName());
+
+    /**
+     * Partition collection into collections of taille size.
+     *
+     * @param liste  the list to be splitted
+     * @param taille the max size of splitted lists
+     * @return the collection of collections
+     * @throws NumberFormatException the number format exception
+     */
+    public Collection<List<Integer>> partition(final String liste, final String taille) throws NumberFormatException {
+
+        final List<Integer> numberList = new ArrayList();
+        //retrieving string parameters and convert them into numbers
+        final Integer size = Integer.valueOf(taille);
+        final String[] values = liste.replace("{", "").replace("}", "").split(",");
+        for (final String s : values) {
+            numberList.add(Integer.valueOf(s));
+        }
+
+        final AtomicInteger counter = new AtomicInteger();
+        //splitting list into n lists of size elements
+        return numberList.stream()
+                .collect(Collectors.groupingBy(element -> counter.getAndIncrement() / size))
+                .values();
+    }
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(final String[] args) {
+        final ListFormatter listFormatter = new ListFormatter();
+        try {
+            listFormatter.partition(args[0], args[1]);
+        } catch (NumberFormatException e) {
+            logger.log(Level.SEVERE, "invalid parameter " + e.getMessage());
+        }
+    }
+}
--- src/test/java/com/adneom/ListFormatterTest.java
+++ src/test/java/com/adneom/ListFormatterTest.java
@@ -1,93 +1,114 @@
 package com.adneom;
 
+import com.adneom.formatter.ListFormatter;
 import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.InvalidPropertiesFormatException;
 import java.util.List;
-import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertThat;
 
+/**
+ * The List formatter test.
+ */
 public class ListFormatterTest {
 
-    ListFormatter listFormatter;
+    /**
+     * The basic list for tests
+     */
+    public static final String LISTE = "{1,2,3,4,5}";
+    /**
+     * The List formatter.
+     */
+    public ListFormatter listFormatter;
 
+    /**
+     * Sets up.
+     *
+     * @throws Exception the exception
+     */
     @Before
     public void setUp() throws Exception {
         listFormatter = new ListFormatter();
     }
 
+    /**
+     * Partition five element list into two element lists test.
+     */
     @Test
-    public void partitionFiveElementListIntoTwoElementListsTest() throws InvalidPropertiesFormatException {
-        int[] liste = new int[]{1,2,3,4,5};
-        int taille = 2;
-        Collection<List<Integer>> partition = listFormatter.partition(liste, taille);
-        List<Integer> integers1 = new ArrayList<>();
+    public void partitionFiveElementListIntoTwoElementListsTest() {
+        final Collection<List<Integer>> partition = listFormatter.partition(LISTE, "2");
+        final List<Integer> integers1 = new ArrayList<>();
         integers1.add(1);
         integers1.add(2);
-        List<Integer> integers2 = new ArrayList<>();
+        final List<Integer> integers2 = new ArrayList<>();
         integers2.add(3);
         integers2.add(4);
-        List<Integer> integers3 = new ArrayList<>();
+        final List<Integer> integers3 = new ArrayList<>();
         integers3.add(5);
-        assertThat(partition, CoreMatchers.hasItems(integers1,integers2,integers3));
+        assertThat("the two collections should be the same", partition, CoreMatchers.hasItems(integers1, integers2, integers3));
     }
 
+    /**
+     * Partition five element list into three element lists test.
+     */
     @Test
-    public void partitionFiveElementListIntoThreeElementListsTest() throws InvalidPropertiesFormatException {
-        int[] liste = new int[]{1,2,3,4,5};
-        int taille = 3;
-        Collection<List<Integer>> partition = listFormatter.partition(liste, taille);
-        List<Integer> integers1 = new ArrayList<>();
+    public void partitionFiveElementListIntoThreeElementListsTest() {
+        final Collection<List<Integer>> partition = listFormatter.partition(LISTE, "3");
+        final List<Integer> integers1 = new ArrayList<>();
         integers1.add(1);
         integers1.add(2);
         integers1.add(3);
-        List<Integer> integers2 = new ArrayList<>();
+        final List<Integer> integers2 = new ArrayList<>();
         integers2.add(4);
         integers2.add(5);
-        assertThat(partition, CoreMatchers.hasItems(integers1,integers2));
+        assertThat("the two collections should be the same", partition, CoreMatchers.hasItems(integers1, integers2));
     }
 
+    /**
+     * Partition five element list into one element lists test.
+     */
     @Test
-    public void partitionFiveElementListIntoOneElementListsTest() throws InvalidPropertiesFormatException {
-        int[] liste = new int[]{1,2,3,4,5};
-        int taille = 1;
-        Collection<List<Integer>> partition = listFormatter.partition(liste, taille);
-        List<Integer> integers1 = new ArrayList<>();
+    public void partitionFiveElementListIntoOneElementListsTest() {
+        final Collection<List<Integer>> partition = listFormatter.partition(LISTE, "1");
+        final List<Integer> integers1 = new ArrayList<>();
         integers1.add(1);
-        List<Integer> integers2 = new ArrayList<>();
+        final List<Integer> integers2 = new ArrayList<>();
         integers2.add(2);
-        List<Integer> integers3 = new ArrayList<>();
+        final List<Integer> integers3 = new ArrayList<>();
         integers3.add(3);
-        List<Integer> integers4 = new ArrayList<>();
+        final List<Integer> integers4 = new ArrayList<>();
         integers4.add(4);
-        List<Integer> integers5 = new ArrayList<>();
+        final List<Integer> integers5 = new ArrayList<>();
         integers5.add(5);
-        assertThat(partition, CoreMatchers.hasItems(integers1,integers2,integers3,integers4,integers5));
+        assertThat("the two collections should be the same", partition, CoreMatchers.hasItems(integers1, integers2, integers3, integers4, integers5));
     }
 
-    @Test
-    public void partitionHugeElementListIntoOneElementListsTest() throws InvalidPropertiesFormatException {
-        IntStream liste = IntStream.range(0,Integer.MAX_VALUE);
-        int taille = 1;
-        Collection<List<Integer>> partition = listFormatter.partition(liste.toArray(), taille);
-        for(int i=0;i<liste.count();i++){
-
-        }
-        List<Integer> integers1 = new ArrayList<>();
-        integers1.add(1);
-        List<Integer> integers2 = new ArrayList<>();
-        integers2.add(2);
-        List<Integer> integers3 = new ArrayList<>();
-        integers3.add(3);
-        List<Integer> integers4 = new ArrayList<>();
-        integers4.add(4);
-        List<Integer> integers5 = new ArrayList<>();
-        integers5.add(5);
-        assertThat(partition, CoreMatchers.hasItems(integers1,integers2,integers3,integers4,integers5));
+    /**
+     * Partition invalid size parameters test.
+     */
+    @Test(expected = NumberFormatException.class)
+    public void partitionInvalidSizeParametersTest() {
+        listFormatter.partition(LISTE, "toto");
     }
+
+    /**
+     * Partition invalid list parameters test.
+     */
+    @Test(expected = NumberFormatException.class)
+    public void partitionInvalidListParametersTest() {
+        listFormatter.partition("wrong list", "2");
+    }
+
+    /**
+     * Partition invalid empty list parameters test.
+     */
+    @Test(expected = NumberFormatException.class)
+    public void partitionInvalidEmptyListParametersTest() {
+        listFormatter.partition("", "2");
+    }
+
 }
